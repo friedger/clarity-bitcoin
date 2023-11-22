1445dda3a998618621bc782c0545be8d22f58e4a
friedger <mail@friedger.de>
2023-11-22 13:29:18 +0100

    chore: make one test per function

--- tests/clar.test.ts
+++ tests/clar.test.ts
@@ -3,41 +3,38 @@ import { Cl, cvToString } from "@stacks/transactions";
 import { describe, expect, it } from "vitest";
 
 const accounts = simnet.getAccounts();
+simnet.getContractsInterfaces().forEach((contract, name) => {
+  if (!name.endsWith("_test")) {
+    return;
+  }
+  describe(name, () => {
+    const prepare = contract.functions.findIndex((f) => f.name === "prepare");
+    let block: ParsedTransactionResult[];
 
-describe("Clarity tests", () => {
-  it("Run all test functions", () => {
-    simnet.getContractsInterfaces().forEach((contract, name) => {
-      if (!name.endsWith("_test")) {
+    contract.functions.forEach((fn) => {
+      if (!fn.name.startsWith("test-")) {
         return;
       }
 
-      simnet.getContractAST(name).expressions.forEach((expr) => {
-        //console.log(JSON.stringify(expr));
-      });
-
-      const prepare = contract.functions.findIndex((f) => f.name === "prepare");
-
-      let block: ParsedTransactionResult[];
-
-      contract.functions.forEach((fn) => {
-        if (!fn.name.startsWith("test")) {
-          return;
-        }
-
+      it(fn.name, () => {
         if (prepare) {
           block = simnet.mineBlock([
             tx.callPublicFn(name, "prepare", [], accounts.get("deployer")!),
             tx.callPublicFn(name, fn.name, [], accounts.get("deployer")!),
           ]);
-          console.log(name, fn.name, cvToString(block[1].result));
           expect(block[0].result).toBeOk(Cl.bool(true));
-          expect(block[1].result).toBeOk(Cl.bool(true));
+          expect(
+            block[1].result,
+            `${name}, ${fn.name}, ${cvToString(block[0].result)}`
+          ).toBeOk(Cl.bool(true));
         } else {
           block = simnet.mineBlock([
             tx.callPublicFn(name, fn.name, [], accounts.get("deployer")!),
           ]);
-          console.log(name, fn.name, cvToString(block[0].result));
-          expect(block[0].result).toBeOk(Cl.bool(true));
+          expect(
+            block[0].result,
+            `${name}, ${fn.name}, ${cvToString(block[0].result)}`
+          ).toBeOk(Cl.bool(true));
         }
       });
     });
