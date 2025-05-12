bc820b735275b9e12c06f85b655d51b9d2f422c0
friedger <mail@friedger.de>
2025-05-12 10:56:24 +0200

    chore: improve tests

---
+++ tests/btc-stx-swap_cachedData.ts
@@ -0,0 +1,92 @@
+export const txs = {
+  f5a993361e1db33c3b2323e39eda6c8ee70bc08da1429d0a2f81063751e55c73: {
+    in_active_chain: true,
+    txid: 'f5a993361e1db33c3b2323e39eda6c8ee70bc08da1429d0a2f81063751e55c73',
+    hash: 'f5a993361e1db33c3b2323e39eda6c8ee70bc08da1429d0a2f81063751e55c73',
+    version: 1,
+    size: 191,
+    vsize: 191,
+    weight: 764,
+    locktime: 0,
+    vin: [
+      {
+        txid: '21c1e589abc135a0efbc70fe4fc74badf4958d664d4fd9e006eb75b6111603fb',
+        vout: 0,
+        scriptSig: {
+          asm: '304402207ce0dcbdb1b3790831fc294a451bfc329b129588bef8766697f589c37666d85f0220135b39800f5d1ec2c329fd125be71cba4132b429cb3827815a2af9c21ee36f69[ALL] 03e6a7489ea5fe06f9363830466067e34c574b44c485cb8ee9a4fc98276dc5a5c8',
+          hex: '47304402207ce0dcbdb1b3790831fc294a451bfc329b129588bef8766697f589c37666d85f0220135b39800f5d1ec2c329fd125be71cba4132b429cb3827815a2af9c21ee36f69012103e6a7489ea5fe06f9363830466067e34c574b44c485cb8ee9a4fc98276dc5a5c8',
+        },
+        sequence: 4294967295,
+      },
+    ],
+    vout: [
+      {
+        value: 0.00046847,
+        n: 0,
+        scriptPubKey: {
+          asm: 'OP_DUP OP_HASH160 7bb1218a48c58c35c3537e6560e804023ee73106 OP_EQUALVERIFY OP_CHECKSIG',
+          desc: 'addr(1CH2JsgrMXmXUZWokyekHPZq4dYmeNBQjv)#pk83yamv',
+          hex: '76a9147bb1218a48c58c35c3537e6560e804023ee7310688ac',
+          address: '1CH2JsgrMXmXUZWokyekHPZq4dYmeNBQjv',
+          type: 'pubkeyhash',
+        },
+      },
+    ],
+    hex: '0100000001fb031611b675eb06e0d94f4d668d95f4ad4bc74ffe70bcefa035c1ab89e5c121000000006a47304402207ce0dcbdb1b3790831fc294a451bfc329b129588bef8766697f589c37666d85f0220135b39800f5d1ec2c329fd125be71cba4132b429cb3827815a2af9c21ee36f69012103e6a7489ea5fe06f9363830466067e34c574b44c485cb8ee9a4fc98276dc5a5c8ffffffff01ffb60000000000001976a9147bb1218a48c58c35c3537e6560e804023ee7310688ac00000000',
+    blockhash: '00000000000000000001c55626b85b4b3ecb33f67645356a2b01f4dfba893679',
+    confirmations: 13157,
+    time: 1739225363,
+    blocktime: 1739225363,
+  },
+  c1de234c01ecc47906117d012865ce3dabbbb081dc0309a74dbbae45e427aadc: {
+    in_active_chain: true,
+    txid: 'c1de234c01ecc47906117d012865ce3dabbbb081dc0309a74dbbae45e427aadc',
+    hash: '2c6ac48882f2d1f8a972061557c4fcce054ae233ef897719574aafd88da92076',
+    version: 2,
+    size: 223,
+    vsize: 142,
+    weight: 565,
+    locktime: 0,
+    vin: [
+      {
+        txid: '1de6bcf900cd34344b93c2e1f424ed5d1c75bae2d46a8399e15384932b6ae8f6',
+        vout: 1,
+        scriptSig: { asm: '', hex: '' },
+        txinwitness: [
+          '304402205edad21077602766ffbce1276b6a3b69577a521b28dc18d8062303723ac91cc802200ec9ea72f62069b1e5817a999fa3d31acf0f655a159691feb8161fb33b93f3fa01',
+          '03997651fec067eda2c430bfe548f65575737c9b892d8b529ae9dc0dfcb5c5898a',
+        ],
+        sequence: 4294967293,
+      },
+    ],
+    vout: [
+      {
+        value: 0.00028293,
+        n: 0,
+        scriptPubKey: {
+          asm: 'OP_HASH160 c5beca99b2b4c558b297ed9134142f4a3873f4e9 OP_EQUAL',
+          desc: 'addr(3Kibbkftm4XzW6zAaUPrAX3Xx9TjnxAK8f)#hdjt0l30',
+          hex: 'a914c5beca99b2b4c558b297ed9134142f4a3873f4e987',
+          address: '3Kibbkftm4XzW6zAaUPrAX3Xx9TjnxAK8f',
+          type: 'scripthash',
+        },
+      },
+      {
+        value: 0.2056008,
+        n: 1,
+        scriptPubKey: {
+          asm: '0 b84ef1e7e398d56fa88a356df3139ff997114f04',
+          desc: 'addr(bc1qhp80relrnr2kl2y2x4klxyullxt3zncyj9nd3c)#3cnwxrlh',
+          hex: '0014b84ef1e7e398d56fa88a356df3139ff997114f04',
+          address: 'bc1qhp80relrnr2kl2y2x4klxyullxt3zncyj9nd3c',
+          type: 'witness_v0_keyhash',
+        },
+      },
+    ],
+    hex: '02000000000101f6e86a2b938453e199836ad4e2ba751c5ded24f4e1c2934b3434cd00f9bce61d0100000000fdffffff02856e00000000000017a914c5beca99b2b4c558b297ed9134142f4a3873f4e987d0b8390100000000160014b84ef1e7e398d56fa88a356df3139ff997114f040247304402205edad21077602766ffbce1276b6a3b69577a521b28dc18d8062303723ac91cc802200ec9ea72f62069b1e5817a999fa3d31acf0f655a159691feb8161fb33b93f3fa012103997651fec067eda2c430bfe548f65575737c9b892d8b529ae9dc0dfcb5c5898a00000000',
+    blockhash: '00000000000000000001c55626b85b4b3ecb33f67645356a2b01f4dfba893679',
+    confirmations: 13157,
+    time: 1739225363,
+    blocktime: 1739225363,
+  },
+};
